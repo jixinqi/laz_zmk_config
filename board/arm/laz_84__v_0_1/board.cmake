4502d7a9575462d79c7bbaf8df28a99c019a255f
jixinqi <fuyinyuezhang@gmail.com>
2023-05-11 21:59:42 +0800

    1

--- board/arm/laz_84__v_0_1/board.cmake
+++ board/arm/laz_84__v_0_1/board.cmake
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "-c SWD -p stm32f103c8t6 -w $<TARGET_FILE:app>")
+board_runner_args(jlink "--device=STM32F103C8" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
