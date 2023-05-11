bffa6be2f3d4067cdb15a1a474a3f6d56f318e9a
jixinqi <fuyinyuezhang@gmail.com>
2023-05-11 22:03:30 +0800

    1

--- board/arm/laz_84__v_0_1/board.cmake
+++ board/arm/laz_84__v_0_1/board.cmake
@@ -5,3 +5,7 @@ board_runner_args(jlink "--device=STM32F103C8" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 
+# run command:
+# cd zmk/app
+# west build -b laz_84__v_0_1
+# west flash -r openocd
