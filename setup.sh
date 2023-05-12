bb302e20be557605fb3be7a6ed406330e66b612e
jixinqi <fuyinyuezhang@gmail.com>
2023-05-13 02:33:40 +0800

    1

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ ZMK_PROJECT_PATH="$HOME/zmk"
 
 ln -s -f \
     ${LAZ_PROJECT_PATH}/board/arm/laz_84__v_0_1 \
-    ${ZMK_PROJECT_PATH}/app/boards/arm/laz_84__v_0_1
+    ${ZMK_PROJECT_PATH}/app/boards/arm/
